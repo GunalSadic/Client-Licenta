93134e9d2af8c085ce9819e1a724195438c0ac04
Your Name <sadicgunal20@stud.ase.ro>
2023-06-02 17:36:48 +0300

    Just in case of need to revert

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LoginForm from './LoginForm';
 import { Box } from '@mui/system';
 import { Grid } from '@mui/material';
+
 function LoginPage() {
     return (
       
