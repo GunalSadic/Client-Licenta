0196dafbdf545c8c0c8445378b7cf2d23f535bfb
Your Name <sadicgunal20@stud.ase.ro>
2023-07-15 01:54:49 +0300

    lightChanges

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import AuthenticationContext from './authentication/AuthenticationContext';
 import { set } from 'react-hook-form';
 import { getClaims } from './authentication/HandleJWT';
 import MatchmakingLobby from './components/MatchmakingLobby';
+import Demo from './components/LearnChessground';
 function App() {
 
   const [claims,setClaims] = useState([]);
@@ -23,8 +24,9 @@ function App() {
         <Route path="/Login" element = {<LoginPage/>}></Route>
         <Route path ="/Register" element = {<RegistrationPage />}></Route>
         <Route path="/Leaderboards" element = {<LeaderboardPage/>}></Route>
-        <Route path="/Table" element = {<MatchmakingLobby/>}></Route>
+        <Route path="/Table" element = {<Demo/>}></Route>
         <Route path="/AvatarCreator" element = {<AvatarCreator/>}></Route>
+        <Route path="/Test" element = {<Demo/>}></Route>
       </Route>
     </Routes>
     </AuthenticationContext.Provider>
--- src/components/MatchmakingLobby.js
+++ src/components/MatchmakingLobby.js
@@ -1,13 +1,19 @@
-import { HubConnectionBuilder, LogLevel, HttpTransportType} from '@microsoft/signalr';
-import { useState, useMemo, useEffect } from 'react';
+import { HubConnectionBuilder, LogLevel} from '@microsoft/signalr';
+import { useState, useMemo, useEffect, useRef } from 'react';
 import { Button,Box, Typography,Grid } from '@mui/material';
 import { getClaims } from '../authentication/HandleJWT';
 import ChessGame from './ChessBoard';
 import { Chess } from 'chess.js';
 import Chessboard from 'chessboardjsx';
 import { Avatar } from '@readyplayerme/visage';
+import Chessground from "@react-chess/chessground/build/esm"
+import { toDests } from '../ChessLogic/Logic';
+import "../styles/chessground.base.css"
+import "../styles/chessground.brown.css"
+import "../styles/chessground.cburnett.css"
+import { Chessground as ChessgroundApi } from 'chessground';
 function MatchmakingLobby(){
-    
+    ChessgroundApi
     const [connection,setConnection] = useState();  
     const [queuePlayerCount, setQueuePlayerCount] = useState();
     const userEmail = getClaims().filter(x => x.name === 'email')[0].value;
@@ -15,11 +21,10 @@ function MatchmakingLobby(){
     const chess = useMemo(() => new Chess(), []);
     const [fen, setFen] = useState(chess.fen());
     const [color,setColor] = useState('white');
-    const [yourTurn,setYourTurn] = useState();
     const [buttonDisabled, setButtonDisabled] = useState(false);
     const [user1AvatarData, setUser1AvatarData] = useState(null);
     const [user2AvatarData, setUser2AvatarData] = useState(null);
-  
+    const chessgroundRef = useRef(null);
     useEffect(() => {
       // Fetch avatar data for the first user
       fetch('https://models.readyplayer.me/6470fcc1d71bf8b85c3b006d.glb')
@@ -42,19 +47,22 @@ function MatchmakingLobby(){
             setColor(data.player2Color);
             setOpponentEmail(data.player1Email);
         }
+        
     }
     const handleClick = async () =>{
         try{
             setButtonDisabled(true);
+            
             const token = localStorage.getItem('token')
             const connection = new HubConnectionBuilder().
             withUrl("https://localhost:7244/GameHub",{
                 accessTokenFactory: ()=> {return `Bearer ${token}`},  
               }).
+
             configureLogging(LogLevel.Information).build(); 
             connection.on("QueuePlayerCountUpdate",(playerCount)=>{console.log(playerCount)});
             connection.on("MatchFound",(response) => MatchFound(response));
-            connection.on("ReceiveMove",(move) => handleMove(move,false));
+            connection.on("ReceiveMove",(origin,dest) => {handleMove(origin,dest,false)});
             connection.on("ReceiveMessage", (message) => console.log(message));
             await connection.start();
             await connection.invoke("JoinQueue",userEmail);
@@ -64,21 +72,18 @@ function MatchmakingLobby(){
             console.log(error)
         }
     }
-    const handleMove = (move,sendOver = true) => {
+    const handleMove = (origin,dest,sendOver = true) => {
         try{
+            var move;
+            move = origin+dest;
             if(chess.turn() === color[0] || sendOver == false){
-                const moveChecker = new Chess(chess.fen());
-                moveChecker.move(move);
                 chess.move(move);
                 setFen(chess.fen());
-                if(chess.isGameOver)
-                    console.log("GameOver");
-                else if(chess.isDraw)
-                    console.log("Draw");
                 if(sendOver === true)
                 {
                     let moveJSON = JSON.stringify({
-                        move: move.from + move.to,
+                        origin : origin,
+                        dest : dest,
                         userEmail: userEmail,
                         opponentEmail: opponentEmail
                     })
@@ -94,7 +99,6 @@ function MatchmakingLobby(){
     }
     if(opponentEmail){
         return(
-
                 <Grid
                 container
                 spacing={0}
@@ -105,19 +109,16 @@ function MatchmakingLobby(){
                 >
                 <Typography variant='h6'>{opponentEmail}</Typography>
                 <Avatar modelSrc={user2AvatarData}/>
-                <Chessboard
-                  orientation = {color}
-                  width={400}
-                  position={fen}
-                  onDrop={(move) =>
-                    handleMove({
-                      from: move.sourceSquare,
-                      to: move.targetSquare,
-                      promotion: "q"
-                    })
-                  }
-                >
-                </Chessboard>
+              
+                <Chessground
+                ref={chessgroundRef}
+                width={400}
+                height={400}
+                config={
+                    {addPieceZIndex: true, orientation: color, events: {move: handleMove}, 
+                    movable:{dests: toDests(chess), free: false, color: color}}
+                }
+            />
                 <Typography variant='h6'>{userEmail}</Typography>
                 <Avatar modelSrc={user1AvatarData}/>
             </Grid>
